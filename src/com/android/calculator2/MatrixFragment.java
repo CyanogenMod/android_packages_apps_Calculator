e969fc629d1e05ea8b7b0bba7571dced39091b84
Will Harmon <xlythe@gmail.com>
2012-06-17 21:25:12 -0400

    Calculator: New Features
    Squashed commit of the following patches, with modifications to build
    under the Android build system and return to the original namespace:
    
    start of new features
    
    (most buttons are set to do nothing though)
    
    delete whole function (log, sin, etc)
    
    gitignore
    
    modulo working
    
    graphing started
    
    I'll probably make it it's own panel, but for now...
    
    graphs functions (mostly)
    
    graph, matrix panels in menu
    
    graphing actually decent now (few bugs though)
    
    bug fixes for graphing (graph on del, on start up, fix for negative numbers)
    
    wolfram alpha for functions
    
    graph works in separate thread, updated icons
    
    graphing optimizations
    
    bug fixes in graphing, buttons in matrices
    
    new ui for adding matrices
    
    click to remove matrix
    
    save matrices on rotation
    
    depricated code, begone!
    
    solving matrices works (screw order of operations)
    
    does not actually display the result though...
    
    okay, probably important - displays matrix answers
    
    bugfixes
    
    multiple sizes for matrices
    
    catches for matrix exceptions
    
    fixes for line breaks in graphs
    
    bug fix for tablets and back button
    
    tablet panels
    
    updated graphics
    
    bumped up graph resolution (might be too slow)
    
    changed package to com.android2.calculator3
    
    matrix scrollable, dot product, cross product
    
    spacing added
    
    hex and bin support (no button yet)
    
    new page with Hex digits, a few optimizations
    
    dec, hex, and bin don't work properly atm
    
    calculate in hex, bin
    
    null pointers occasionally, selected input should stay highlighted (ie:
    dec button should be blue if set to dec), and it should save the mode
    when minimized
    
    save mode on close
    
    see selected mode
    
    set selected mode on load
    
    doesnt save mode, buggy
    
    fix bug where loading would fail if activity finished
    
    fix crash when converting
    
    rearranged hex page, graphing fixed
    
    update to 1.2
    
    display descriptions on long press
    
    pulldown image added, error handled better
    
    bar is draggable
    
    bar snaps to top/bottom, goes up on back
    
    limit drag height
    
    better handling for dragging
    
    show history
    
    increase text size in history
    
    curse you, padding, curse yoooou
    
    remove pages
    
    calculate rationals in hex/bin
    
    click to open/close history
    
    remove function page, support for complex numbers
    
    fixed history getting overwritten
    
    copy from history
    
    hide paste when nothing to paste
    
    comas in answer, fix rotation bug
    
    fixed crash with decimals in hex/bin
    
    removed outdated code
    
    fix image stretch
    
    matrix add icon swapped out
    
    manually merged a few changes from upstream
    
    icon is uniquely ours
    
    limit max width in history
    
    bugfix with history height and keyboards
    
    add matrix button changed back to match better
    
    removed comas in results, localized equation when graphing
    
    (bugfixes for languages other than English)
    
    attempt at fixing mismatched formatting
    
    zooming in graph shouldn't have been enabled yet
    
    removed hardcoded values in graph
    
    preparation for zooming
    
    bugfix for null graphs
    
    swapped out matrix add button
    
    translation fix for nl
    
    zoom enabled on graph
    
    bugfixes for graphing
    
    Change-Id: I7d7d7a1ec2cf33ac20b2bf265884eeb4c4ebbad7

---
+++ src/com/android/calculator2/MatrixFragment.java
@@ -0,0 +1,257 @@
+package com.android.calculator2;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+public class MatrixFragment extends Fragment{
+    Context mContext;
+    ScrollView s;
+
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mContext = getActivity();
+        setRetainInstance(true);
+        
+        if (s == null) {
+            
+            s = (ScrollView) inflater.inflate(R.layout.matrix_panel, null);
+            final LinearLayout matrices = (LinearLayout) s.findViewById(R.id.matrices);
+            ImageButton i = (ImageButton) s.findViewById(R.id.matrixAdd);
+            i.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    View view = inflater.inflate(R.layout.matrix, null);
+                    final LinearLayout theMatrix = (LinearLayout) view.findViewById(R.id.theMatrix);
+                    final LinearLayout matrixPopup = (LinearLayout) view.findViewById(R.id.matrixPopup);
+                    final LinearLayout matrixButtons = (LinearLayout) matrixPopup.findViewById(R.id.matrixButtons);
+                    
+                    builder.setView(view);
+                    final AlertDialog alertDialog = builder.create();
+                    WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                    lp.copyFrom(alertDialog.getWindow().getAttributes());
+                    lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                    lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                    
+                    final ColorButton ok = (ColorButton) view.findViewById(R.id.ok);
+                    ok.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            for (int i=0; i<theMatrix.getChildCount(); i++) {
+                                LinearLayout layout = (LinearLayout) theMatrix.getChildAt(i);
+                                for(int j=0; j<layout.getChildCount(); j++) {
+                                    EditText view = (EditText) layout.getChildAt(j);
+                                    if(view.getText().toString().equals("")) {
+                                        view.requestFocus();
+                                        return;
+                                    }
+                                }
+                                for(int j=0; j<layout.getChildCount(); j++) {
+                                    EditText view = (EditText) layout.getChildAt(j);
+                                    view.setOnFocusChangeListener(new OnFocusChangeListener() {
+                                        @Override
+                                        public void onFocusChange(View v, boolean hasFocus) {
+                                            if(hasFocus) {
+                                                View theMatrix = (View) v.getParent().getParent();
+                                                ViewGroup parent = (ViewGroup) theMatrix.getParent();
+                                                parent.removeView(theMatrix);
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                            theMatrix.setFocusable(true);
+                            theMatrix.setFocusableInTouchMode(true);
+                            theMatrix.requestFocus();
+                            ViewGroup parent = (ViewGroup) theMatrix.getParent();
+                            parent.removeView(theMatrix);
+                            matrices.addView(theMatrix, matrices.getChildCount()-1);
+                            alertDialog.dismiss();
+                        }
+                    });
+
+                    final ColorButton plus = (ColorButton) view.findViewById(R.id.matrixPlus);
+                    plus.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            plus.setLayoutParams(new ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT));
+                            plus.setOnClickListener(new OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    matrices.removeView(v);
+                                }
+                            });
+                            matrixButtons.removeView(plus);
+                            matrices.addView(plus, matrices.getChildCount()-1);
+                            alertDialog.dismiss();
+                        }
+                    });
+                    
+                    final ColorButton mul = (ColorButton) view.findViewById(R.id.matrixMul);
+                    mul.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mul.setLayoutParams(new ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT));
+                            mul.setOnClickListener(new OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    matrices.removeView(v);
+                                }
+                            });
+                            matrixButtons.removeView(mul);
+                            matrices.addView(mul, matrices.getChildCount()-1);
+                            alertDialog.dismiss();
+                        }
+                    });
+                    
+                    final ColorButton dot = (ColorButton) view.findViewById(R.id.matrixDot);
+                    dot.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dot.setLayoutParams(new ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT));
+                            dot.setPadding(15, 10, 15, 10);
+                            dot.setOnClickListener(new OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    matrices.removeView(v);
+                                }
+                            });
+                            matrixButtons.removeView(dot);
+                            matrices.addView(dot, matrices.getChildCount()-1);
+                            alertDialog.dismiss();
+                        }
+                    });
+                    
+                    final ColorButton cross = (ColorButton) view.findViewById(R.id.matrixCross);
+                    cross.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            cross.setLayoutParams(new ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                                    ViewGroup.LayoutParams.WRAP_CONTENT));
+                            cross.setPadding(15, 10, 15, 10);
+                            cross.setOnClickListener(new OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    matrices.removeView(v);
+                                }
+                            });
+                            matrixButtons.removeView(cross);
+                            matrices.addView(cross, matrices.getChildCount()-1);
+                            alertDialog.dismiss();
+                        }
+                    });
+                    DisplayMetrics metrics = new DisplayMetrics();
+                    getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                    final float logicalDensity = metrics.density;
+                    final LinearLayout grip_bar_port = (LinearLayout) view.findViewById(R.id.grip_bar_port);
+                    grip_bar_port.setOnTouchListener(new OnTouchListener() {
+                        long distance = 0;
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                distance = 0;
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                if((event.getX()-0.5)/logicalDensity>75+distance) {
+                                    int rows = ((LinearLayout) theMatrix.getChildAt(0)).getChildCount();
+                                    if(rows > 1) {
+                                        for(int i=0; i<theMatrix.getChildCount(); i++) {
+                                            LinearLayout l = (LinearLayout) theMatrix.getChildAt(i);
+                                            l.removeViewAt(l.getChildCount()-1);
+                                        }
+                                        distance += 75;
+                                    }
+                                }
+                                else if((event.getX()-0.5)/logicalDensity<-75+distance) {
+                                    for(int i=0; i<theMatrix.getChildCount(); i++) {
+                                        LinearLayout l = (LinearLayout) theMatrix.getChildAt(i);
+                                        EditText e = (EditText) inflater.inflate(R.layout.single_matrix_input_box, null);
+                                        e.setWidth((int) (75*logicalDensity+0.5));
+                                        e.setHeight((int) (100*logicalDensity+0.5));
+                                        l.addView(e);
+                                    }
+                                    distance -= 75;
+                                }
+                                break;
+                            }
+                            return true;
+                        }
+                    });
+                    final LinearLayout grip_bar_land = (LinearLayout) view.findViewById(R.id.grip_bar_land);
+                    grip_bar_land.setOnTouchListener(new OnTouchListener() {
+                        long distance = 0;
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            switch (event.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                distance = 0;
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+                                if((event.getY()-0.5)/logicalDensity>100+distance) {
+                                    int rows = ((LinearLayout) theMatrix.getChildAt(0)).getChildCount();
+                                    LinearLayout l = new LinearLayout(mContext);
+                                    l.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+                                    l.setOrientation(LinearLayout.HORIZONTAL);
+                                    for(int i=0; i<rows; i++) {
+                                        EditText e = (EditText) inflater.inflate(R.layout.single_matrix_input_box, null);
+                                        e.setWidth((int) (75*logicalDensity+0.5));
+                                        e.setHeight((int) (100*logicalDensity+0.5));
+                                        
+                                        l.addView(e);
+                                    }
+                                    theMatrix.addView(l);
+                                    distance += 100;
+                                }
+                                else if((event.getY()-0.5)/logicalDensity<-100+distance) {
+                                    int columns = theMatrix.getChildCount();
+                                    if(columns > 1) {
+                                        theMatrix.removeViewAt(0);
+                                        distance -= 100;
+                                    }
+                                }
+                                break;
+                            }
+                            return true;
+                        }
+                    });
+
+                    alertDialog.getWindow().setAttributes(lp);
+                    alertDialog.show();
+                }
+            });
+        }
+        else{
+             ViewGroup parent = (ViewGroup) s.getParent();
+             parent.removeView(s);
+        }
+        
+        return s;
+    }
+}
