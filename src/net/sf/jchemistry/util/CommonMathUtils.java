5f140ac5e8344787373bccf9e99d3a7111a4989a
Will Harmon <xlythe@gmail.com>
2012-06-17 21:25:12 -0400

    Calculator: New Features
    Squashed commit of the following patches, with modifications to build
    under the Android build system and return to the original namespace:
    
    start of new features
    
    (most buttons are set to do nothing though)
    
    delete whole function (log, sin, etc)
    
    gitignore
    
    modulo working
    
    graphing started
    
    I'll probably make it it's own panel, but for now...
    
    graphs functions (mostly)
    
    graph, matrix panels in menu
    
    graphing actually decent now (few bugs though)
    
    bug fixes for graphing (graph on del, on start up, fix for negative numbers)
    
    wolfram alpha for functions
    
    graph works in separate thread, updated icons
    
    graphing optimizations
    
    bug fixes in graphing, buttons in matrices
    
    new ui for adding matrices
    
    click to remove matrix
    
    save matrices on rotation
    
    depricated code, begone!
    
    solving matrices works (screw order of operations)
    
    does not actually display the result though...
    
    okay, probably important - displays matrix answers
    
    bugfixes
    
    multiple sizes for matrices
    
    catches for matrix exceptions
    
    fixes for line breaks in graphs
    
    bug fix for tablets and back button
    
    tablet panels
    
    updated graphics
    
    bumped up graph resolution (might be too slow)
    
    changed package to com.android2.calculator3
    
    matrix scrollable, dot product, cross product
    
    spacing added
    
    hex and bin support (no button yet)
    
    new page with Hex digits, a few optimizations
    
    dec, hex, and bin don't work properly atm
    
    calculate in hex, bin
    
    null pointers occasionally, selected input should stay highlighted (ie:
    dec button should be blue if set to dec), and it should save the mode
    when minimized
    
    save mode on close
    
    see selected mode
    
    set selected mode on load
    
    doesnt save mode, buggy
    
    fix bug where loading would fail if activity finished
    
    fix crash when converting
    
    rearranged hex page, graphing fixed
    
    update to 1.2
    
    display descriptions on long press
    
    pulldown image added, error handled better
    
    bar is draggable
    
    bar snaps to top/bottom, goes up on back
    
    limit drag height
    
    better handling for dragging
    
    show history
    
    increase text size in history
    
    curse you, padding, curse yoooou
    
    remove pages
    
    calculate rationals in hex/bin
    
    click to open/close history
    
    remove function page, support for complex numbers
    
    fixed history getting overwritten
    
    copy from history
    
    hide paste when nothing to paste
    
    comas in answer, fix rotation bug
    
    fixed crash with decimals in hex/bin
    
    removed outdated code
    
    fix image stretch
    
    matrix add icon swapped out
    
    manually merged a few changes from upstream
    
    icon is uniquely ours
    
    limit max width in history
    
    bugfix with history height and keyboards
    
    add matrix button changed back to match better
    
    removed comas in results, localized equation when graphing
    
    (bugfixes for languages other than English)
    
    attempt at fixing mismatched formatting
    
    zooming in graph shouldn't have been enabled yet
    
    removed hardcoded values in graph
    
    preparation for zooming
    
    bugfix for null graphs
    
    swapped out matrix add button
    
    translation fix for nl
    
    zoom enabled on graph
    
    bugfixes for graphing
    
    Change-Id: I7d7d7a1ec2cf33ac20b2bf265884eeb4c4ebbad7

---
+++ src/net/sf/jchemistry/util/CommonMathUtils.java
@@ -0,0 +1,114 @@
+/*
+ * jChemistry
+ * Copyright (C) 2011 Philippe T. Pinard
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.jchemistry.util;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
+/**
+ * Utilities for the Apache common math library.
+ * 
+ * @author Philippe T. Pinard
+ */
+public final class CommonMathUtils {
+
+    /**
+     * Converts a <code>Vector3D</code> to a <code>RealVector</code>.
+     * 
+     * @param v
+     *            a vector 3D
+     * @return a real vector
+     */
+    public static RealVector toRealVector(Vector3D v) {
+        return new ArrayRealVector(
+                new double[] { v.getX(), v.getY(), v.getZ() });
+    }
+
+
+
+    /**
+     * Converts a <code>RealVector</code> to a <code>Vector3D</code>.
+     * 
+     * @param v
+     *            a real vector
+     * @return a vector 3D
+     */
+    public static Vector3D toVector3D(RealVector v) throws IllegalArgumentException{
+        if (v.getDimension() != 3)
+            throw new IllegalArgumentException("The vector length ("
+                    + v.getDimension() + ") must be equal to 3.");
+
+        return new Vector3D(v.getEntry(0), v.getEntry(1), v.getEntry(2));
+    }
+    
+    /**
+     * Converts a <code>RealVector</code> to a <code>RealMatrix</code>.
+     * 
+     * @param v
+     *            a real vector
+     * @return a real matrix
+     */
+    public static RealMatrix toRealMatrix(RealVector v) {
+        double[] values = new double[v.getDimension()];
+        for(int i=0;i<v.getDimension();i++) {
+            values[i] = v.getEntry(i);
+        }
+        return new Array2DRowRealMatrix(values);
+    }
+    
+    /**
+     * Converts a <code>Vector3D</code> to a <code>RealMatrix</code>.
+     * 
+     * @param v
+     *            a Vector3D
+     * @return a RealMatrix
+     */
+    public static RealMatrix toRealMatrix(Vector3D v) {
+        double[] values = { v.getX(), v.getY(), v.getZ() };
+        return new Array2DRowRealMatrix(values);
+    }
+
+
+
+    /**
+     * Multiplies the specified matrix by the specified COLUMN vector.
+     * <p/>
+     * The <code>RealMatrix</code> interface only allows multiplication of
+     * matrix by ROW vector.
+     * 
+     * @param m
+     *            matrix
+     * @param v
+     *            vector
+     * @return resultant COLUMN vector
+     */
+    public static RealVector postMultiply(RealMatrix m, RealVector v) {
+        return m.transpose().preMultiply(v);
+    }
+
+
+
+    /**
+     * Constructor to prevent the class to be instantiated.
+     */
+    private CommonMathUtils() {
+    }
+}
