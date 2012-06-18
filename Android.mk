f70ae326ca1a70ce3d762160ff5cc2a272392e46
Will Harmon <xlythe@gmail.com>
2012-06-17 21:25:12 -0400

    start of new features
    (most buttons are set to do nothing though)
    
    delete whole function (log, sin, etc)
    
    gitignore
    
    modulo working
    
    graphing started
    
    I'll probably make it it's own panel, but for now...
    
    graphs functions (mostly)
    
    graph, matrix panels in menu
    
    graphing actually decent now (few bugs though)
    
    bug fixes for graphing (graph on del, on start up, fix for negative numbers)
    
    wolfram alpha for functions
    
    graph works in separate thread, updated icons
    
    graphing optimizations
    
    bug fixes in graphing, buttons in matrices
    
    new ui for adding matrices
    
    click to remove matrix
    
    save matrices on rotation
    
    depricated code, begone!
    
    solving matrices works (screw order of operations)
    
    does not actually display the result though...
    
    okay, probably important - displays matrix answers
    
    bugfixes
    
    multiple sizes for matrices
    
    catches for matrix exceptions
    
    fixes for line breaks in graphs
    
    bug fix for tablets and back button
    
    tablet panels
    
    updated graphics
    
    bumped up graph resolution (might be too slow)
    
    changed package to com.android2.calculator3
    
    matrix scrollable, dot product, cross product
    
    spacing added
    
    hex and bin support (no button yet)
    
    new page with Hex digits, a few optimizations
    
    dec, hex, and bin don't work properly atm
    
    calculate in hex, bin
    
    null pointers occasionally, selected input should stay highlighted (ie:
    dec button should be blue if set to dec), and it should save the mode
    when minimized
    
    save mode on close
    
    see selected mode
    
    set selected mode on load
    
    doesnt save mode, buggy
    
    fix bug where loading would fail if activity finished
    
    fix crash when converting
    
    rearranged hex page, graphing fixed
    
    update to 1.2
    
    display descriptions on long press
    
    pulldown image added, error handled better
    
    bar is draggable
    
    bar snaps to top/bottom, goes up on back
    
    limit drag height
    
    better handling for dragging
    
    show history
    
    increase text size in history
    
    curse you, padding, curse yoooou
    
    remove pages
    
    calculate rationals in hex/bin
    
    click to open/close history
    
    remove function page, support for complex numbers
    
    fixed history getting overwritten
    
    copy from history
    
    hide paste when nothing to paste
    
    comas in answer, fix rotation bug
    
    fixed crash with decimals in hex/bin
    
    removed outdated code
    
    fix image stretch
    
    matrix add icon swapped out
    
    manually merged a few changes from upstream
    
    icon is uniquely ours
    
    limit max width in history
    
    bugfix with history height and keyboards
    
    add matrix button changed back to match better
    
    Change-Id: I7d7d7a1ec2cf33ac20b2bf265884eeb4c4ebbad7

--- Android.mk
+++ Android.mk
@@ -19,7 +19,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
 LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4 guava
+LOCAL_STATIC_JAVA_LIBRARIES += chartengine commons-math3
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
@@ -31,7 +34,7 @@ include $(BUILD_PACKAGE)
 ##################################################
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libarity:arity-2.1.2.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libarity:libs/arity-2.1.6.jar chartengine:libs/achartengine-1.0.0.jar commons-math3:libs/commons-math3-3.0.jar
 
 include $(BUILD_MULTI_PREBUILT)
 
