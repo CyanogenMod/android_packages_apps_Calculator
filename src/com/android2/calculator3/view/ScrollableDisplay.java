5fa1f663f5ca45e7be8eb92137886f6f25b4b60c
Will <xlythe@gmail.com>
2013-02-01 15:19:52 -0500

    framework for fixing Gravity.RIGHT

--- src/com/android2/calculator3/view/ScrollableDisplay.java
+++ src/com/android2/calculator3/view/ScrollableDisplay.java
@@ -2,6 +2,7 @@ package com.android2.calculator3.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnLongClickListener;
@@ -27,5 +28,19 @@ public class ScrollableDisplay extends HorizontalScrollView implements OnLongCli
     public boolean onTouchEvent(MotionEvent ev) {
         super.onTouchEvent(ev);
         return false;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        // HorizontalScrollView is broken for Gravity.RIGHT. So we're fixing it.
+        ScrollableDisplay.LayoutParams p = (LayoutParams) getView().getLayoutParams();
+        int horizontalGravity = p.gravity & Gravity.HORIZONTAL_GRAVITY_MASK;
+        if(horizontalGravity == Gravity.RIGHT) {
+            // TODO Fix layouts with Gravity.RIGHT
+            super.onLayout(changed, left, top, right, bottom);
+        }
+        else {
+            super.onLayout(changed, left, top, right, bottom);
+        }
     }
 }
