0acbf268adab71ee88f9b8125b67b71f46070dea
Will <xlythe@gmail.com>
2013-05-05 22:38:17 -0400

    vibrate when del long pressed

--- src/com/android2/calculator3/EventListener.java
+++ src/com/android2/calculator3/EventListener.java
@@ -17,6 +17,7 @@
 package com.android2.calculator3;
 
 import android.content.Context;
+import android.os.Vibrator;
 import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
 import android.view.View;
@@ -286,6 +287,8 @@ public class EventListener implements View.OnKeyListener, View.OnClickListener, 
         switch(view.getId()) {
         case R.id.del:
             mHandler.onClear();
+            Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(100);
         }
         if(view.getTag() != null) {
             String text = (String) view.getTag();
