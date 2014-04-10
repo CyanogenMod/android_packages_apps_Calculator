afd483f2ca21453b60ffffb117320e6f6803ceba
Xlythe <xlythe@gmail.com>
2014-04-09 23:28:21 -0700

    switch to android studio

---
+++ ThemeEngine/src/main/java/com/xlythe/engine/theme/ThemedLinearLayout.java
@@ -0,0 +1,69 @@
+package com.xlythe.engine.theme;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class ThemedLinearLayout extends LinearLayout {
+    public ThemedLinearLayout(Context context) {
+        super(context);
+        setup(context, null);
+    }
+
+    public ThemedLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setup(context, attrs);
+    }
+
+    @SuppressLint("NewApi")
+    public ThemedLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setup(context, attrs);
+    }
+
+    private void setup(Context context, AttributeSet attrs) {
+        if(attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.theme);
+            if(a != null) {
+                // Get divider
+                setDivider(Theme.get(a.getResourceId(R.styleable.theme_themeDivider, 0)));
+
+                // Get background
+                setBackground(Theme.get(a.getResourceId(R.styleable.theme_themeBackground, 0)));
+
+                a.recycle();
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    public void setDivider(Theme.Res res) {
+        if(res != null) {
+            if(Theme.DRAWABLE.equals(res.getType())) {
+                if(android.os.Build.VERSION.SDK_INT >= 11) {
+                    setDividerDrawable(Theme.getDrawable(getContext(), res.getName()));
+                }
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @SuppressWarnings("deprecation")
+    public void setBackground(Theme.Res res) {
+        if(res != null) {
+            if(Theme.COLOR.equals(res.getType())) {
+                setBackgroundColor(Theme.getColor(getContext(), res.getName()));
+            }
+            else if(Theme.DRAWABLE.equals(res.getType())) {
+                if(android.os.Build.VERSION.SDK_INT < 16) {
+                    setBackgroundDrawable(Theme.getDrawable(getContext(), res.getName()));
+                }
+                else {
+                    setBackground(Theme.getDrawable(getContext(), res.getName()));
+                }
+            }
+        }
+    }
+}
---
+++ ThemeEngine/src/main/java/com/xlythe/engine/theme/ThemedScrollView.java
@@ -0,0 +1,58 @@
+package com.xlythe.engine.theme;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+public class ThemedScrollView extends ScrollView {
+    public ThemedScrollView(Context context) {
+        super(context);
+        setup(context, null);
+    }
+
+    public ThemedScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setup(context, attrs);
+    }
+
+    public ThemedScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setup(context, attrs);
+    }
+
+    private void setup(Context context, AttributeSet attrs) {
+        if(attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.theme);
+            if(a != null) {
+                // Get background
+                setBackground(Theme.get(a.getResourceId(R.styleable.theme_themeBackground, 0)));
+
+                if("none".equals(Theme.getString(getContext(), "requiresFadingEdge"))) {
+                    setVerticalFadingEdgeEnabled(false);
+                }
+
+                a.recycle();
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @SuppressWarnings("deprecation")
+    public void setBackground(Theme.Res res) {
+        if(res != null) {
+            if(Theme.COLOR.equals(res.getType())) {
+                setBackgroundColor(Theme.getColor(getContext(), res.getName()));
+            }
+            else if(Theme.DRAWABLE.equals(res.getType())) {
+                if(android.os.Build.VERSION.SDK_INT < 16) {
+                    setBackgroundDrawable(Theme.getDrawable(getContext(), res.getName()));
+                }
+                else {
+                    setBackground(Theme.getDrawable(getContext(), res.getName()));
+                }
+            }
+        }
+    }
+}
