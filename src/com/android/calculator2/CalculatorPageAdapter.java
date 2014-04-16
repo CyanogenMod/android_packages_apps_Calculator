001e7247eefd56d72478beadaf9cbcfdd6f577ce
Xlythe <xlythe@gmail.com>
2014-04-16 00:00:25 -0400

    Calculator: New Features Part 6
    - Floating Calculator
    - New theme engine (& store)
    - Google Glass support
    - Graphs rewritten from scratch to support equations such as circles
    - Panels can be rearranged
    - Infinite scrolling option
    - Details in top left. Quick toggle for Radians/Degrees
    - Button vibration option
    - Widget UI updated to match stock apps
    - Settings cleaned up
    
    Change-Id: Id096d35b854833044f7168e7a16cd95f8ef8bbd1

--- src/com/android/calculator2/CalculatorPageAdapter.java
+++ src/com/android/calculator2/CalculatorPageAdapter.java
@@ -10,58 +10,62 @@ import android.view.ViewGroup;
 import com.android.calculator2.BaseModule.Mode;
 
 public abstract class CalculatorPageAdapter extends PagerAdapter {
-    @Override
-    public void startUpdate(View container) {}
+	@Override
+	public void startUpdate(View container) {}
 
-    public abstract View getViewAt(int position);
+	public abstract View getViewAt(int position);
 
-    @Override
-    public Object instantiateItem(View container, int position) {
-        View v = getViewAt(position);
-        ((ViewGroup) container).addView(v);
+	@Override
+	public Object instantiateItem(View container, int position) {
+		View v = getViewAt(position);
+		((ViewGroup) container).addView(v);
 
-        return v;
-    }
+		return v;
+	}
 
-    @Override
-    public void destroyItem(View container, int position, Object object) {
-        ((ViewGroup) container).removeView((View) object);
-    }
+	@Override
+	public void destroyItem(View container, int position, Object object) {
+		((ViewGroup) container).removeView((View) object);
+	}
 
-    @Override
-    public void finishUpdate(View container) {}
+	@Override
+	public void finishUpdate(View container) {}
 
-    @Override
-    public boolean isViewFromObject(View view, Object object) {
-        return view == object;
-    }
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return view == object;
+	}
 
-    @Override
-    public Parcelable saveState() {
-        return null;
-    }
+	@Override
+	public Parcelable saveState() {
+		return null;
+	}
 
-    @Override
-    public void restoreState(Parcelable state, ClassLoader loader) {}
+	@Override
+	public void restoreState(Parcelable state, ClassLoader loader) {}
 
-    protected void applyBannedResourcesByPage(Logic logic, ViewGroup page, Mode baseMode) {
-        // Enable
-        for(Mode key : logic.mBaseModule.mBannedResources.keySet()) {
-            if(baseMode.compareTo(key) != 0) {
-                List<Integer> resources = logic.mBaseModule.mBannedResources.get(key);
-                for(Integer resource : resources) {
-                    final int resId = resource.intValue();
-                    View v = page.findViewById(resId);
-                    if(v != null) v.setEnabled(true);
-                }
-            }
-        }
-        // Disable
-        List<Integer> resources = logic.mBaseModule.mBannedResources.get(baseMode);
-        for(Integer resource : resources) {
-            final int resId = resource.intValue();
-            View v = page.findViewById(resId);
-            if(v != null) v.setEnabled(false);
-        }
-    }
+	protected void applyBannedResourcesByPage(Logic logic, View page, Mode baseMode) {
+		// Enable
+		for(Mode key : logic.getBaseModule().mBannedResources.keySet()) {
+			if(baseMode.compareTo(key) != 0) {
+				List<Integer> resources = logic.getBaseModule().mBannedResources.get(key);
+				for(Integer resource : resources) {
+					final int resId = resource.intValue();
+					View v = page.findViewById(resId);
+					if(v != null) v.setEnabled(true);
+				}
+			}
+		}
+		// Disable
+		List<Integer> resources = logic.getBaseModule().mBannedResources.get(baseMode);
+		for(Integer resource : resources) {
+			final int resId = resource.intValue();
+			View v = page.findViewById(resId);
+			if(v != null) v.setEnabled(false);
+		}
+	}
+
+	public abstract Iterable<View> getViewIterator();
+
+	public abstract List<Page> getPages();
 }
