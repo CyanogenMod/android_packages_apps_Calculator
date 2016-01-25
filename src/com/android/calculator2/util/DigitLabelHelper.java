ab2fe59b382ee9a9eb4fa1d0a51b42c8950def5b
Danesh M <daneshm90@gmail.com>
2016-01-25 14:35:43 -0800

    Calculator : Fix widget and floating calculator
    Due to the switch from hardcoded strings to ICU, the widget
    and the floating calculator need to be updated.
    
    CYNGNOS-1702
    
    Change-Id: Ia1f5439fc3afe375011ff841caa220d35c5328df

---
+++ src/com/android/calculator2/util/DigitLabelHelper.java
@@ -0,0 +1,76 @@
+package com.android.calculator2.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import com.android.calculator2.R;
+
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public final class DigitLabelHelper {
+
+    private static final String UNICODE_LOCALE_KEY = "nu";
+    private static final String UNICODE_LOCALE_VALUE = "latn";
+
+    private static DigitLabelHelper sInstance;
+    //private final Context mContext;
+    //private final Resources mResources;
+    private int mLastLocaleHash;
+    private DecimalFormatSymbols mDecFormatSymbols;
+
+    private static final int[] sDigitIds = new int[] {
+            R.id.digit0,
+            R.id.digit1,
+            R.id.digit2,
+            R.id.digit3,
+            R.id.digit4,
+            R.id.digit5,
+            R.id.digit6,
+            R.id.digit7,
+            R.id.digit8,
+            R.id.digit9,
+            R.id.dec_point
+    };
+
+    public interface DigitLabelHelperCallback {
+        void setDigitText(int id, String text);
+    }
+
+    public synchronized static DigitLabelHelper getInstance() {
+        if (sInstance == null) {
+            sInstance = new DigitLabelHelper();
+        }
+        return sInstance;
+    }
+
+    private DecimalFormatSymbols getDecimalFormatForCurrentLocale(Context context) {
+        Resources resources = context.getResources();
+        Locale locale = resources.getConfiguration().locale;
+        DecimalFormatSymbols decimalFormatSymbols = mDecFormatSymbols;
+        if (locale.hashCode() != mLastLocaleHash) {
+            if (!resources.getBoolean(R.bool.use_localized_digits)) {
+                locale = new Locale.Builder()
+                        .setLocale(locale)
+                        .setUnicodeLocaleKeyword(UNICODE_LOCALE_KEY, UNICODE_LOCALE_VALUE)
+                        .build();
+            }
+            mLastLocaleHash = locale.hashCode();
+            decimalFormatSymbols = DecimalFormatSymbols.getInstance(locale);
+            mDecFormatSymbols = decimalFormatSymbols;
+        }
+        return decimalFormatSymbols;
+    }
+
+    public void getTextForDigits(Context context, DigitLabelHelperCallback callback) {
+        final DecimalFormatSymbols symbols = getDecimalFormatForCurrentLocale(context);
+        final char zeroDigit = symbols.getZeroDigit();
+        for (int i = 0; i < sDigitIds.length; i++) {
+            int id = sDigitIds[i];
+            if (id == R.id.dec_point) {
+                callback.setDigitText(id, String.valueOf(symbols.getDecimalSeparator()));
+            } else {
+                callback.setDigitText(id, String.valueOf((char) (zeroDigit + i)));
+            }
+        }
+    }
+}
