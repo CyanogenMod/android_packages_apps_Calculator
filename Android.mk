58285c361dcf158bfeb359d61f9e29918866463b
Ethan Chen <intervigil@gmail.com>
2016-04-08 11:46:00 -0700

    Calculator: Override ExactCalculator
    * We don't include Calculator as part of the default manifest, and we
      want the old Calculator package to override ExactCalculator in cases
      we do include it.
    
    RM-232
    Change-Id: If8ef12e8f2017a3ee75ebfd22a744b54e634298f

--- Android.mk
+++ Android.mk
@@ -26,6 +26,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SDK_VERSION := current
 
+LOCAL_OVERRIDES_PACKAGES := ExactCalculator
 LOCAL_PACKAGE_NAME := Calculator
 
 res_dir := res ../../../prebuilts/sdk/current/support/v7/appcompat/res \
